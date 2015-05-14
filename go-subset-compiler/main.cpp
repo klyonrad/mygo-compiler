1e9e7aa6423f544cefd17a23355cdf91302741ce
KLuka <klukasocial@gmail.com>
2015-05-14 17:30:27 +0200

    rename lexer in main
    one-letter-variables are the devil.

--- go-subset-compiler/main.cpp
+++ go-subset-compiler/main.cpp
@@ -8,13 +8,13 @@ using namespace std;
 int main(){
     string program;
     cout << "tinyGo Parser v0.1:\n" << endl;
-    Lexer* l = new Lexer();
-    Interpreter i(l);
+    Lexer* myLexer = new Lexer();
+    Interpreter i(myLexer);
     while(true){
         try{
             cout << "tinyGo> ";
             getline(cin, program);
-            l->setProgram(program);
+            myLexer->setProgram(program);
             i.start();
         }catch(const char* c){
             cout.flush();
